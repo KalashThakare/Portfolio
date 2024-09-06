56eab6c43e2484ccaac2d1340934616e6a2eca02
KalashT <kalashthakare898@gmail.com>
2024-09-06 19:00:36 +0530

    contactform update

--- app/contact/contactform.jsx
+++ app/contact/contactform.jsx
@@ -4,18 +4,47 @@ import {Button} from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import {Textarea} from "@/components/ui/textarea";
 import {Select,SelectContent,SelectGroup,SelectItem,SelectLabel,SelectTrigger,SelectValue} from "@/components/ui/select";
+import { useState } from "react";
+
+const initValues={
+    firstname:"",
+    lastname:"",
+    email:"",
+    number:"",
+};
+
+const initState={ values:initValues};
 
 export const ContactForm = () => {
+    const [state,setState]=useState(initState);
+
+    const{values}=state;
+
+    const handleChange=({target})=>setState((prev)=>({
+        ...prev,
+        values:{
+            ...prev.values,
+            [target.name]:target.value,
+        },
+    }));
+
+    const onSubmit=async()=>{
+        setState((prev)=>({
+            ...prev,
+            
+        }));
+    };
+
   return (
     <div className=" xl:w-[54%] order-2 xl:order-none justify-start">
                     <form action="/submit" method="post" className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
                         <h3 className="text-4xl text-accent">Let's work together</h3>
                         <p className="text-white/60 ">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus tempore est id odit maxime nulla?</p>
                         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                            <Input type="firstname" name="firstName" placeholder="Firstname" />
-                            <Input type="lastname" name="LastName" placeholder="Lastname" />
-                            <Input type="email" name="Email" placeholder="Email address" />
-                            <Input type="phone" name="number" placeholder="Phone number" />
+                            <Input type="firstname" name="firstname" placeholder="Firstname" value={values.firstname} onChange={handleChange}/>
+                            <Input type="lastname" name="lastname" placeholder="Lastname" value={values.lastname} onChange={handleChange}/>
+                            <Input type="email" name="email" placeholder="Email address" value={values.email} onChange={handleChange}/>
+                            <Input type="phone" name="number" placeholder="Phone number" value={values.number} onChange={handleChange}/>
                         </div>
                         <Select>
                             <SelectTrigger className="w-full">
@@ -29,10 +58,10 @@ export const ContactForm = () => {
                                 </SelectGroup>
                             </SelectContent>
                         </Select>
-                        <Textarea className="h-[200px]" placeholder="Type your message here.">                       
+                        <Textarea className="h-[200px]" placeholder="Type your message here." type="text" name="message" value={values.message} onChange={handleChange}>                       
                         </Textarea>
                         <Button size="" className="max-w-28 rounded-xl text-lg ">
-                            Send 
+                            Send
                         </Button>
                     </form>
                 </div>
